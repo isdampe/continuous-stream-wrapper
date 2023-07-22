658d7b2d826258d8ebf37a190978b5443d2bfa42
Richard Denton <isdampe@gmail.com>
2023-07-22 15:06:20 +1000

    new: add expunge recordings

---
+++ expunge-recordings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ $# -ne 2 ]; then
+	echo "Usage: ./expunge-recordings.sh [ip-address] [number-of-days-to-retain]"
+	exit 1
+fi
+
+remoteAddr=$1
+numberOfDays=$2
+
+# check if ./data exists
+if [ ! -d "./data" ]; then
+	exit 1
+fi
+
+find ./data -name "$remoteAddr-*.mkv" -mtime +$numberOfDays -exec rm {} \;
